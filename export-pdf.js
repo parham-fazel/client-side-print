c14cfa491ef5e2c3b12da48509a91c1400bcfc2b
Parham <parham.fazel@spatialvison.com.au>
2017-05-10 17:42:25 +1000

    cleanup - removed screen size dependency, added portrait and landscape oprions

--- export-pdf.js
+++ export-pdf.js
@@ -1,100 +1,178 @@
-var raster = new ol.layer.Tile({
-	source: new ol.source.OSM()
-});
-
-var format = new ol.format.WKT();
-var feature = format.readFeature(
-	'POLYGON((10.689697265625 -25.0927734375, 34.595947265625 ' +
-	'-20.1708984375, 38.814697265625 -35.6396484375, 13.502197265625 ' +
-	'-39.1552734375, 10.689697265625 -25.0927734375))');
-feature.getGeometry().transform('EPSG:4326', 'EPSG:3857');
-
-var vector = new ol.layer.Vector({
-	source: new ol.source.Vector({
-		features: [feature]
-	})
-});
-
-
-var map = new ol.Map({
-	layers: [raster, vector],
-	target: 'map',
-	controls: ol.control.defaults({
-		attributionOptions: /** @type {olx.control.AttributionOptions} */ ({
-			collapsible: false
-		})
-	}),
-	view: new ol.View({
-		center: [0, 0],
-		zoom: 2
-	})
-});
-
-
-var dims = {
-	a0: [1189, 841],
-	a1: [841, 594],
-	a2: [594, 420],
-	a3: [420, 297],
-	a4: [297, 210],
-	a5: [210, 148]
-};
-
-var loading = 0;
-var loaded = 0;
-
-var exportButton = document.getElementById('export-pdf');
-
-exportButton.addEventListener('click', function() {
+(function (ol, $, jsPDF) {
 	
-	exportButton.disabled = true;
-	document.body.style.cursor = 'progress';
-	
-	var format = document.getElementById('format').value;
-	var resolution = document.getElementById('resolution').value;
-	var dim = dims[format];
-	var width = Math.round(dim[0] * resolution / 25.4);
-	var height = Math.round(dim[1] * resolution / 25.4);
-	var size = /** @type {ol.Size} */ (map.getSize());
-	var extent = map.getView().calculateExtent(size);
-	
-	var source = raster.getSource();
-	
-	var tileLoadStart = function() {
-		++loading;
-	};
-	
-	var tileLoadEnd = function() {
-		++loaded;
-		if (loading === loaded) {
-			var canvas = this;
-			window.setTimeout(function() {
-				loading = 0;
-				loaded = 0;
-				var data = canvas.toDataURL('image/png');
-				var pdf = new jsPDF('landscape', undefined, format);
-				pdf.addImage(data, 'JPEG', 0, 0, dim[0], dim[1]);
-				pdf.save('map.pdf');
-				source.un('tileloadstart', tileLoadStart);
-				source.un('tileloadend', tileLoadEnd, canvas);
-				source.un('tileloaderror', tileLoadEnd, canvas);
-				map.setSize(size);
-				map.getView().fit(extent);
-				map.renderSync();
-				exportButton.disabled = false;
-				document.body.style.cursor = 'auto';
-			}, 100);
-		}
-	};
-	
-	map.once('postcompose', function(event) {
-		source.on('tileloadstart', tileLoadStart);
-		source.on('tileloadend', tileLoadEnd, event.context.canvas);
-		source.on('tileloaderror', tileLoadEnd, event.context.canvas);
+	var raster = new ol.layer.Tile({
+		source: new ol.source.OSM()
 	});
 	
-	map.setSize([width, height]);
-	map.getView().fit(extent);
-	map.renderSync();
+	var dims = {
+		a0: [1189, 841],
+		a1: [841, 594],
+		a2: [594, 420],
+		a3: [420, 297],
+		a4: [297, 210],
+		a5: [210, 148]
+	};
 	
-}, false);
+	var loading = 0;
+	var loaded = 0;
+	var canvas;
+	var map;
+	var originalMapSize;
+	var originalMapExtent;
+	
+	function createMap() {
+		var map = new ol.Map({
+			layers: [raster],
+			target: 'map',
+			controls: ol.control.defaults({
+				attributionOptions: /** @type {olx.control.AttributionOptions} */ ({
+					collapsible: false
+				})
+			}),
+			view: new ol.View({
+				center: [0, 0],
+				zoom: 2
+			})
+		});
+		return map;
+	}
+	
+	function addVectorData(map) {
+		var format = new ol.format.WKT();
+		var feature = format.readFeature(
+			'POLYGON((10.689697265625 -25.0927734375, 34.595947265625 ' +
+			'-20.1708984375, 38.814697265625 -35.6396484375, 13.502197265625 ' +
+			'-39.1552734375, 10.689697265625 -25.0927734375))');
+		feature.getGeometry().transform('EPSG:4326', 'EPSG:3857');
+		
+		var vector = new ol.layer.Vector({
+			source: new ol.source.Vector({
+				features: [feature]
+			})
+		});
+		
+		map.addLayer(vector);
+	}
+	
+	function init() {
+		map = createMap();
+		addVectorData(map);
+		addTouchCalss();
+		initExportOnClick(map);
+		addTileLoadEventListeners(map, raster);
+		map.once('postcompose', function (event) {
+			canvas = event.context.canvas;
+		});
+	}
+	
+	function addTouchCalss() {
+		if (ol.has.TOUCH) {
+			$('#olCustomControls').addClass('ol-touch');
+		}
+	}
+	
+	function tileLoadEnd() {
+		++loaded;
+	}
+	
+	function exportMapImage() {
+		return canvas.toDataURL('image/png');
+	}
+	
+	function createPDF() {
+		var pc = printConfigs();
+		var data = exportMapImage();
+		var pdf = new jsPDF(pc.orientation, undefined, pc.format);
+		pdf.addImage(data, 'JPEG', 0, 0, pc.dim[0], pc.dim[1]);
+		pdf.save('map.pdf');
+	}
+	
+	function tileLoadStart() {
+		++loading;
+	}
+	
+	function addTileLoadEventListeners(map, raster) {
+		var source = raster.getSource();
+		map.once('postcompose', function (event) {
+			source.on('tileloadstart', tileLoadStart);
+			source.on('tileloadend', tileLoadEnd, event.context.canvas);
+			source.on('tileloaderror', tileLoadEnd, event.context.canvas);
+		});
+	}
+	
+	function removeTileLoadeventListeners(raster) {
+		var source = raster.getSource();
+		source.un('tileloadstart', tileLoadStart);
+		source.un('tileloadend', tileLoadEnd, canvas);
+		source.un('tileloaderror', tileLoadEnd, canvas);
+	}
+	
+	function initExportOnClick(map) {
+		var exportButton = document.getElementById('export-pdf');
+		exportButton.addEventListener('click', function (event) {
+			exportOnClick(map);
+		}, false);
+	}
+	
+	function printConfigs() {
+		var format = document.getElementById('format').value;
+		var resolution = document.getElementById('resolution').value;
+		var orientation = document.getElementById('orientation').value;
+		var dim = dims[format];
+		return {dim: dim, resolution: resolution, format: format, orientation: orientation};
+	}
+	
+	function exportOnClick(map) {
+		var exportButton = document.getElementById('export-pdf');
+		var pc = printConfigs();
+		var width = Math.round(pc.dim[0] * pc.resolution / 25.4);
+		var height = Math.round(pc.dim[1] * pc.resolution / 25.4);
+		originalMapSize = /** @type {ol.Size} */ (map.getSize());
+		originalMapExtent = map.getView().calculateExtent(originalMapSize);
+		exportButton.disabled = true;
+		document.body.style.cursor = 'progress';
+		updateMapSizeForPrint(map, width, height, originalMapExtent);
+		listenToTileLoad();
+	}
+	
+	function updateMapSizeForPrint(map, width, height, extent) {
+		map.setSize([width, height]);
+		map.getView().fit(extent);
+		map.renderSync();
+	}
+	
+	function resetMapSize(map, size, extent) {
+		map.setSize(size);
+		map.getView().fit(extent);
+		map.renderSync();
+	}
+	
+	function onTilesLoadDone(map, size, extent) {
+		var exportButton = document.getElementById('export-pdf');
+		window.setTimeout(function () {
+			loading = 0;
+			loaded = 0;
+			createPDF();
+			removeTileLoadeventListeners(raster);
+			resetMapSize(map, size, extent);
+			exportButton.disabled = false;
+			document.body.style.cursor = 'auto';
+		}, 100);
+	}
+	
+	function listenToTileLoad() {
+		if (loading === loaded) {
+			return onTilesLoadDone(map, originalMapSize, originalMapExtent);
+		}
+		var interval = setInterval(function () {
+			if (loading === loaded) {
+				clearInterval(interval);
+				interval = undefined;
+				return onTilesLoadDone(map, originalMapSize, originalMapExtent);
+			}
+		}, 1000)
+	}
+	
+	init();
+	
+})(ol, jQuery, jsPDF);
